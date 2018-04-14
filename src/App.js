d723f2d864245d6805ce33e6f146155c7879d0eb
Tyjoo26 <Tyjoo21@gmail.com>
2018-04-14 11:46:11 -0600

    Abstracted welcome content to separate component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,16 @@
 import React, { Component } from 'react';
 import logo from './rent_mark.png'
-import banner from './aussie.jpg'
 import './App.css';
 import Header from './header.js'
-import {FormControl, FormGroup, ControlLabel, Form, Col, Button} from 'react-bootstrap';
+import Welcome from './welcome.js'
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <div
-        className="hero-image"
-        style={{backgroundImage: `url(${banner})`}}
-        alt="banner">
-          <p
-          className="welcome">
-          Resident Portal
-          </p>
-        </div>
-        <section id="welcome-body">
-          <div id="welcome-header">
-            {`Access Your Account`}
-          </div>
-          <div className="welcome-content">
-            <Form horizontal>
-              <FormGroup controlId="formHorizontalEmail">
-                <Col componentClass={ControlLabel} sm={2}>
-                  Email
-                </Col>
-                <Col sm={8}>
-                  <FormControl type="email" placeholder="Email" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup controlId="formHorizontalPassword">
-                <Col componentClass={ControlLabel} sm={2}>
-                  Password
-                </Col>
-                <Col sm={8}>
-                  <FormControl type="password" placeholder="Password" />
-                </Col>
-              </FormGroup>
-
-              <FormGroup>
-                <Col smOffset={2} sm={7}>
-                  <Button type="submit">Sign in</Button>
-                </Col>
-              </FormGroup>
-            </Form>;
-          </div>
-        </section>
+        <Welcome />
       </div>
     );
   }
