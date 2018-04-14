305ff35757c39ee0b5f6dd63fedc051eb22ed446
Tyjoo26 <Tyjoo21@gmail.com>
2018-04-14 11:21:52 -0600

    Import bootstrap react and implement react form for loggin in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './rent_mark.png'
 import banner from './aussie.jpg'
 import './App.css';
 import Header from './header.js'
+import {FormControl, FormGroup, ControlLabel, Form, Col, Button} from 'react-bootstrap';
 
 class App extends Component {
   render() {
@@ -12,16 +13,44 @@ class App extends Component {
         <div
         className="hero-image"
         style={{backgroundImage: `url(${banner})`}}
-        alt="banner" />
+        alt="banner">
           <p
           className="welcome">
           Resident Portal
           </p>
-        <div className="welcome-content">
-          <p className="welcome">
-            {`Log in, tell us what you need and we'll get it done.`}
-          </p>
         </div>
+        <section id="welcome-body">
+          <div id="welcome-header">
+            {`Access Your Account`}
+          </div>
+          <div className="welcome-content">
+            <Form horizontal>
+              <FormGroup controlId="formHorizontalEmail">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Email
+                </Col>
+                <Col sm={8}>
+                  <FormControl type="email" placeholder="Email" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup controlId="formHorizontalPassword">
+                <Col componentClass={ControlLabel} sm={2}>
+                  Password
+                </Col>
+                <Col sm={8}>
+                  <FormControl type="password" placeholder="Password" />
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Col smOffset={2} sm={7}>
+                  <Button type="submit">Sign in</Button>
+                </Col>
+              </FormGroup>
+            </Form>;
+          </div>
+        </section>
       </div>
     );
   }
