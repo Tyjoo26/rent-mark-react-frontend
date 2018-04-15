0ed00507a97348a5c87850337ea284ba66a54add
Tyjoo26 <Tyjoo21@gmail.com>
2018-04-15 13:02:51 -0600

    Pass props from app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import logo from './rent_mark.png'
 import './App.css';
 import Header from './header.js'
 import Welcome from './welcome.js'
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      isLoggedIn: false
+    }
+  }
   render() {
     return (
       <div className="App">
-        <Header />
-        <Welcome />
+        <Header
+        isLoggedIn={this.state.isLoggedIn}/>
+        <Welcome isLoggedIn={this.state.isLoggedIn}/>
       </div>
     );
   }
