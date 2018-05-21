68cc8eaa22899e84fbd92ad99cb76786b6980b65
Tyjoo26 <Tyjoo21@gmail.com>
2018-05-21 13:31:16 -0600

    remove react bootstrap form, going to handroll form to work on authorization authentication"

--- src/welcome.js
+++ src/welcome.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import banner from './aussie.jpg'
 import './App.css';
-import Header from './header.js'
-import {FormControl, FormGroup, ControlLabel, Form, Col, Button} from 'react-bootstrap';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
-import tenantDashboard from './tenantDashboard.js'
+// import Header from './header.js'
+// import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+// import tenantDashboard from './tenantDashboard.js'
 
 
 class Welcome extends Component {
 
 
   logIn = (e) => {
-    console.log(e.target.parentNode)
+    e.preventDefault();
+    console.log(e.target)
   }
 
 
@@ -35,31 +35,7 @@ class Welcome extends Component {
           {`Access your account!`}
         </div>
         <div className="welcome-content">
-          <Form horizontal onSubmit={this.logIn}>
-            <FormGroup controlId="formHorizontalEmail">
-              <Col componentClass={ControlLabel} sm={2}>
-                Email
-              </Col>
-              <Col sm={8}>
-                <FormControl type="email" placeholder="Email" />
-              </Col>
-            </FormGroup>
 
-            <FormGroup controlId="formHorizontalPassword">
-              <Col componentClass={ControlLabel} sm={2}>
-                Password
-              </Col>
-              <Col sm={8}>
-                <FormControl type="password" placeholder="Password" />
-              </Col>
-            </FormGroup>
-
-            <FormGroup>
-              <Col smOffset={2} sm={7}>
-                <Button type="submit">Sign in</Button>
-              </Col>
-            </FormGroup>
-          </Form>;
         </div>
       </section>
       </div>
